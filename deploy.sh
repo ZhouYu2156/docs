3a47739755e2dc62f3e0bd99b7c763451f5b27a6
zhouyu2156 <1043744584@qq.com>
2023-12-02 10:21:11 +0800

    auto_deploy

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 set -x  # 这里是为了看错误日志
 
 # 打包项目
-npm run build
+# npm run build
 
 # 进入打包后的文件夹
 # cd ${PWD}/dist/
 
-git init
+# git init
 git add .
-git commit -m 'auto deploy'
+git commit -m 'auto_deploy'
 
-git pull githubware master
+# git pull githubware master:notes
 # 将打包后的文件推送到指定分支
 git push githubware master
