0052b02421c5177cbcd111c9a2c485486ab3d3df
zhouyu2156 <1043744584@qq.com>
2023-12-24 11:09:35 +0800

    提交开发项目

--- src/.vitepress/configuration/modules/socialLinks.ts
+++ src/.vitepress/configuration/modules/socialLinks.ts
@@ -3,13 +3,13 @@ export const socialLinks = [
     icon: {
       svg: '<img style="width: 20px; border-radius: 20%;" src="/gitee.svg">'
     },
-    link: 'https://github.com/ZhouYu2156'
+    link: 'https://gitee.com/zhouyu2156'
   },
   {
     icon: {
       svg: '<img style="width: 20px; border-radius: 20%;" src="/github.svg">'
     },
-    link: 'https://gitee.com/zhouyu2156'
+    link: 'https://github.com/ZhouYu2156'
   },
   {
     icon: { svg: '<img style="width: 20px; border-radius: 20%;" src="/fish.png">' },
