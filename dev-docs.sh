37424c3676943837334b46c04d683a0b9ca91a7d
ZhouYu2156 <1043744584@qq.com>
2024-10-13 15:16:32 +0800

    项目重构完成

--- dev-docs.sh
+++ dev-docs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-read -p "Enter commit message: "
+echo "Commit message: $1"
 
 if [ -z "$1" ]; then
   echo "Commit message cannot be empty"
@@ -25,20 +25,26 @@ fi
 echo "Commit pushed successfully"
 
 
-cd ./notes/dist
-git init
-git branch -m master main
-git add .
-git commit -m "$1"
-git remote add docs git@github.com:ZhouYu2156/zhouyu2156.github.io.git
-git pull docs main --allow-unrelated-histories
 if [ $? -eq 0 ]; then
-  echo "Pushing to docs"
+  cd ./notes/dist
+  git init
+  # git branch -m master main
+  git add .
+  git commit -m "$1"
+  git remote add docs git@github.com:ZhouYu2156/zhouyu2156.github.io.git
+  git pull docs main --allow-unrelated-histories
+  if [ $? -eq 0 ]; then
+    echo "Pushing to docs"
+  else
+    echo "Error pushing to docs"
+    exit 1
+  fi
+  git push -f docs main
 else
-  echo "Error pushing to docs"
+  echo "Error pushing to github"
   exit 1
 fi
-git push -f docs main
+
 
 
 
